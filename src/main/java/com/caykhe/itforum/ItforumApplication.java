d769d5963f38a8e89b387532568f5de9a4804771
Vin <phamvanvinh8a2@gmail.com>
2023-12-14 21:45:54 +0700

    First commit

---
+++ src/main/java/com/caykhe/itforum/ItforumApplication.java
@@ -0,0 +1,13 @@
+package com.caykhe.itforum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItforumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItforumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/caykhe/itforum/ItforumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.caykhe.itforum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItforumApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
