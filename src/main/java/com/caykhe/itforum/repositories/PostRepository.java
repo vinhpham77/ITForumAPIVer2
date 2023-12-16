e443482ad22e72b13afa879009eb80c706f425b6
Vin <phamvanvinh8a2@gmail.com>
2023-12-16 23:59:06 +0700

    get by user, optimize get tags

--- src/main/java/com/caykhe/itforum/repositories/PostRepository.java
+++ src/main/java/com/caykhe/itforum/repositories/PostRepository.java
@@ -2,13 +2,11 @@ package com.caykhe.itforum.repositories;
 
 import com.caykhe.itforum.models.Post;
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
     Page<Post> findByCreatedByUsername(String username, Pageable pageable);
-
     long countByCreatedByUsername(String username);
     Page<Post> findByCreatedByUsernameAndIsPrivateFalse(String username, Pageable pageable);
     long countByCreatedByUsernameAndIsPrivateFalse(String username);
